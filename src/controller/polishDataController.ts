eee2900ad1715f8821199888e3d1fa934f63f2d5
AntoineLbrn <antoinelmh@hotmail.fr>
2026-03-02 23:06:41 +0100

    re-hydrate labels

--- src/controller/polishDataController.ts
+++ src/controller/polishDataController.ts
@@ -5,16 +5,20 @@ export const generateLabelsForCurrentRecord = () => {
     if (currentRecord) {
         configTableRecords.forEach(configRecord => {
             console.log("Processing column labels: ", configRecord.label);
-            console.log("Current state: ", currentRecord[configRecord.label]);
-            currentRecord[configRecord.id].split(";").forEach((conceptId: string) => {
-                console.log("Fetching concept id label: ", conceptId);
-                const idThesaurus = (new URL(conceptId)).searchParams.get("idt");
-                const idConcept = (new URL(conceptId)).searchParams.get("idc");
-                console.log("Extracted idThesaurus and idConcept: ", idThesaurus, idConcept);
-                getConceptLabels(idThesaurus || "", idConcept || "").then(concept => {
-                    console.log("Fetched concept label: ", concept.label);
+            if (!currentRecord[configRecord.label])
+                console.warn('Column does not exist: ', configRecord.label)
+            else {
+                console.log("Current state: ", currentRecord[configRecord.label]);
+                currentRecord[configRecord.id].split(";").forEach((conceptId: string) => {
+                    console.log("Fetching concept id label: ", conceptId);
+                    const idThesaurus = (new URL(conceptId)).searchParams.get("idt");
+                    const idConcept = (new URL(conceptId)).searchParams.get("idc");
+                    console.log("Extracted idThesaurus and idConcept: ", idThesaurus, idConcept);
+                    getConceptLabels(idThesaurus || "", idConcept || "").then(concept => {
+                        console.log("Fetched concept label: ", concept.label);
+                    });
                 });
-            });
+            }
         })
     }
 }
