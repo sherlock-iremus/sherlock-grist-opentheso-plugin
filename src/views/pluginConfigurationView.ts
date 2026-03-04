1f07d5d6eae864ecff477976790fc1e91f958745
AntoineLbrn <antoinelmh@hotmail.fr>
2026-03-04 16:19:58 +0100

    ux

--- src/views/pluginConfigurationView.ts
+++ src/views/pluginConfigurationView.ts
@@ -2,19 +2,15 @@ import { configWarningDiv, contentDiv, configWarningList } from "../views/plugin
 
 export const displayColumnsMissing = (labelMissing: FormattedConfigurationTableRecord[], uriMissing: FormattedConfigurationTableRecord[]) => {
     for (const col of uriMissing) {
-        configWarningList.appendChild((() => {
-            const li = document.createElement("li");
-            li.textContent = `${col.uri} (thésaurus : ${col.thesaurus})`
-            return li;
-        })());
-    }
+        const li = document.createElement("li");
+        li.textContent = `${col.uri} (colonne d'URIs)`
+        configWarningList.appendChild(li);
+    };
     for (const col of labelMissing) {
-        configWarningList.appendChild((() => {
-            const li = document.createElement("li");
-            li.textContent = `${col.label} (thésaurus : ${col.thesaurus})`
-            return li;
-        })());
-    }
+        const li = document.createElement("li");
+        li.textContent = `${col.label} (colonne de libellés)`
+        configWarningList.appendChild(li);
+    };
     configWarningDiv.style.display = "block";
     contentDiv.style.display = "none";
 }
